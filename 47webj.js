3aaf6b0f8f388f99747f5bfed6a413840661f889
shivamkunkolikar <shivam.kunkolikar@gmail.com>
2024-01-05 22:39:53 +0530

    seventh commit

--- 47webj.js
+++ 47webj.js
@@ -54,6 +54,7 @@ function mainGame() {
                 top_pos = top_pos + inr_acc;
                 if (top_pos >= 95 || top_pos <= 0 || left_pos >= 95 || left_pos <= 0) {
                     document.getElementById("square").style.top = `95%`;
+                    localStorage.setItem('hi', `${score + 1}`);
                     clearInterval(game_end);
                 }
                 else {
@@ -65,6 +66,36 @@ function mainGame() {
             document.getElementById("score-num").innerHTML = `${score}`;
         }
     }, 50);
+}
+
+let a = localStorage.getItem('hi');
+console.log(a);
+
+function restartGame() {
+    direction = 0;
+    left_pos = 45;
+    top_pos = 45;
+    inr_acc = 0.4;
+    score = 0;
+    mainGame();
+}
+
+function newGame() {
+    document.getElementById("game-page").style.display = "flex";
+    document.getElementById("start-page").style.animation = "fadinganimation 0.5s ease-in-out 0s 1 ";
+    document.getElementById("game-page").style.animation = "g_an1 1s ease-in-out 0s 1 ";
+    setTimeout(() => {
+        document.getElementById("start-page").style.display = "none"
+    }, 450)
+
+}
+
+function highScore() {
+    document.getElementById("str-btn1").style.display = "none";
+    document.getElementById("str-btn2").style.animation = "s_an2 1s ease-in-out 0s 1 ";
+    setTimeout(() => {
+        document.getElementById("str-btn2").style.height = "400px";
+    }, 1000)
 }
 
 mainGame();
